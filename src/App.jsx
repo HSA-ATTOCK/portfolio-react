2b29c3f295d61e4841d04fff8d41761b53916a28
HSA-ATTOCK <haidersajjadazmat@gmail.com>
2025-08-17 03:14:50 +0500

    updated backend wakeup

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 // src/App.jsx
 import React from "react";
 import "./index.css";
-import { useBackendWakeup } from "./hooks/useBackendWakeup";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
 import About from "./components/About";
@@ -12,7 +11,6 @@ import Contact from "./components/Contact";
 import Footer from "./components/Footer";
 
 function App() {
-  useBackendWakeup();
   return (
     <>
       <Header />
